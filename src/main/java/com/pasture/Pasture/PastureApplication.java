890970c8575672742bd04b9a02b27911d7ea7100
h9g29 <doyouthinkofme@nate.com>
2022-07-07 16:05:14 +0900

    Initial commit

---
+++ src/main/java/com/pasture/Pasture/PastureApplication.java
@@ -0,0 +1,13 @@
+package com.pasture.Pasture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasture/Pasture/PastureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasture.Pasture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
